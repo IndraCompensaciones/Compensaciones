bbd93ed3951950a2a29015b4602932550b593515
mamorera <mamorera@github.com>
2020-11-18 13:06:21 -0500

    paquetes
    paquetes

--- WSCompensaciones/WSCompensaciones-ejb/src/main/java/co/com/claro/compensaciones/NewSessionBean.java
+++ WSCompensaciones/WSCompensaciones-ejb/src/main/java/co/com/claro/compensaciones/NewSessionBean.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package co.co.claro.compensaciones;
+package co.com.claro.compensaciones;
 
 import javax.ejb.Stateless;
 import javax.ejb.LocalBean;
